43aefb41242ba4cce2eb0d546ffd0ed34951a52c
MichalDojcar <md.mamut@seznam.cz>
2013-10-20 11:07:37 +0200

    Routes added to MockDB

--- client/src/com/hackathon/completeroute/dao/mock/MockDB.java
+++ client/src/com/hackathon/completeroute/dao/mock/MockDB.java
@@ -23,8 +23,24 @@ public class MockDB {
         category = new Category("telco", "Telco", "telco");
         Company company1 = new Company("O2", "Telefonica O2", "906112567", "o2_logo");
         company1.setCategory("telco");
-        //Route route1 = new Route(1, "")
-        Company company2 = new Company("Centropol", "Centropol - vitrual operator", "906112567", "centropol_logo");
+        Route route1 = new Route(1,"Faktury, platby a poslední vyúčtování");
+        Route route11 = new Route(1,"Faktury");
+        Route route12 = new Route(2,"Platby");
+        Route route13 = new Route(3,"Poslední vyúčtování");
+        route1.setRoutes(Arrays.asList(route11, route12, route13));
+        Route route2 = new Route(2,"Nastavení stávajících služeb, a roaming");
+        Route route21 = new Route(1,"Nastavení stávajících služeb");
+        Route route22 = new Route(2,"Informace");
+        Route route23 = new Route(3,"Roaming");
+        route2.setRoutes(Arrays.asList(route21, route22, route23));
+        Route route3 = new Route(3,"Aktivace tarifů a služeb");
+        Route route31 = new Route(1,"Aktivace nového tarifu");
+        Route route32 = new Route(2,"Aktivace nové služby");
+        route3.setRoutes(Arrays.asList(route31, route32));
+        Route route4 = new Route(4,"Nahlášení poruchy");
+        Route route5 = new Route(5,"Výpověď služby");
+        company1.setRoutes(Arrays.asList(route1, route2, route3, route4, route5));
+        Company company2 = new Company("Centropol", "Centropol - virtual operator", "906112567", "centropol_logo");
         company2.setCategory("telco");
         category.setCompanies(Arrays.asList(company1, company2));
         categories.add(category);
